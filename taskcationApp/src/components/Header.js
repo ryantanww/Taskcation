4b5f2a82571348d3ad4b7f7cbe94e3c17d6656a8
ryantanww <ryantanww@gmail.com>
2025-03-13 22:17:33 +0800

    removed unnecessary style

--- taskcationApp/src/components/Header.js
+++ taskcationApp/src/components/Header.js
@@ -30,22 +30,12 @@ const styles = StyleSheet.create({
         paddingHorizontal: 10,
         backgroundColor: '#8B4513',
     },
-    // Style for logo
-    logo: {
-        width: 50, 
-        height: 50, 
-    },
     // Style for the header title
     headerTitle: {
         fontSize: 48,
         fontWeight: '800',
         color: '#F5F5DC',
         textAlign: 'center',
-    },
-    // Style for settings
-    settings: {
-        fontSize: 20,
-        color: '#9b59b6',
     },
 });
 
